45d6e9f0e90ae61ff731a1752757e6cf2af80f4c
Jeffin Basil <jeffinbasil484@gmail.com>
2025-07-14 21:59:04 +0530

    Added pages for game, home, and result views

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Game from "./Game";
+import Result from "./Result";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game/:gameId" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
