7de28665de2fb4d4d60f5e26bdb1c54402c5b98c
Red-194 <adithyamanghat@gmail.com>
2025-07-15 19:35:25 +0530

    files

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,26 @@
 from typing import List, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel # type: ignore
 
 class NewGameRequest(BaseModel):
     mode: str
     ai_mode: bool
     depth: Optional[int] = 5
 
-
 class NewGameResponse(BaseModel):
-    board: List[str]
     player_symbol: str
-    player_move: int
-    max_depth: Optional[int]
+    your_turn: bool
+    board: List[str]
     ai_move: Optional[int] = None
-    
+    result: str
+    depth: int
+    ai_enabled: bool
+    mode: str
+
+
+class MoveRequest(BaseModel):
+    board: List[str]
+    player_move: int
+    player_symbol: str
+    ai_enabled: bool
+    depth: int
+    mode: str
